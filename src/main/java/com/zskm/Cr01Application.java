c4ac06f23edeaf0223c51443d6eab1d30bf9cfc3
limozaza <zakboite@gmail.com>
2020-07-29 11:06:08 +0200

    init

---
+++ src/main/java/com/zskm/Cr01Application.java
@@ -0,0 +1,13 @@
+package com.zskm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cr01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cr01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zskm/Cr01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zskm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cr01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
